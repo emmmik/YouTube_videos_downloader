3d751d628440aa2bc36dc116ca6cdedc3ae82d38
emmmik <75755181+emmmik@users.noreply.github.com>
2021-05-23 12:47:01 +0300

    Add files via upload
    The code itself

---
+++ main.py
@@ -0,0 +1,19 @@
+from pytube import YouTube
+
+# Getting a link from a user
+link = input('Insert your link:\n')
+yt = YouTube(link)
+
+# Printing information about the video on the screen
+print('Author:', yt.author)
+print('Name:', yt.title)
+print('Views:', yt.views)
+print('Duration:', str(yt.length // 60) + ':' + str((yt.length % 60) // 10) + str(yt.length % 60))
+
+# Getting the highest resolution stream
+ys = yt.streams.get_highest_resolution()
+
+# Downloading the video
+print('Wait a sec, your video is downloading...')
+ys.download('D:/Useless/Youtube_videos')
+print('Done :)')
